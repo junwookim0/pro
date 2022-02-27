740cd7a1975890bc7b47975977e5d03db7f2eac2
kimjunwoo <ekdlskalrx98@naver.com>
2022-02-27 19:17:27 +0900

    02.27 first

--- 미니프로젝트/js/login.js
+++ 미니프로젝트/js/login.js
@@ -15,16 +15,22 @@ const memoFormShow = document.querySelector("#memo-form");
 function onLoginSumit(event){
     //loginput에서 value 값
     event.preventDefault(); //이벤트를 막는다
-
     //console.log(loginInput.value); //loginInput 안에 있는 속성값을 확인
     const username = loginInput.value;
-    greeting.innerHTML = `반갑습니다. ${username}님`;
-    /*let nik = document.getElementById("Nick").value;
-    nik = "안녕하세요 "+nik+"님";
-    document.getElementById("greeting").innerHTML = nik;*/
-    loginForm.classList.add("hidden");  // loginform 에 css의 hidden 을 html에 추가해준다
-    memoFormShow.classList.remove("hidden");   //메모장 숨김을 제거함 
+    if(username == "그린"){
+        greeting.innerHTML = `반갑습니다. ${username}님`;
+        /*let nik = document.getElementById("Nick").value;
+        nik = "안녕하세요 "+nik+"님";
+        document.getElementById("greeting").innerHTML = nik;*/
+        loginForm.classList.add("hidden");  // loginform 에 css의 hidden 을 html에 추가해준다
+        memoFormShow.classList.remove("hidden");   //메모장 숨김을 제거함 
+    }else {
+        greeting.innerHTML = "잘못입력된 이름입니다";
+    }
+
+    
 }
 
 loginForm.addEventListener("submit",onLoginSumit);
+
 
---
+++ 미니프로젝트/js/memo2.js
@@ -0,0 +1,31 @@
+const memoBoard = document.querySelector("#memo-board");
+
+const memoForm = document.querySelector("#memo-form");
+const memoInput = document.querySelector("#memo-form input");
+
+const savememo = ["메모2 입니다",
+"미리 만들어진 메모입니다","미리 만들어진 메모입니다",
+"미리 만들어진 메모입니다"];
+
+for (let m of savememo){
+    let memo = document.createElement("div");
+    memoBoard.appendChild(memo);
+    memo.innerHTML = m;
+}
+function memoFormSubmit(event){
+    event.preventDefault();
+    console.log(memoInput.value);
+
+    //새로운 요소 (태그)를 만들어서 memo
+    let memo = document.createElement("div");
+
+    // memoBoard에 요소추가
+    memoBoard.appendChild(memo); //appendChild >>메모에 추가
+    //추가된 memo 요소에 html 값 추가
+    memo.innerHTML = memoInput.value;  //사용자 입력값 
+    memoInput.value ="";
+    console.dir(memoInput);
+    memoInput.autofocus = true;  //자동으로 정렬 안써도 기본설정 true
+}
+
+memoForm.addEventListener("submit", memoFormSubmit);
