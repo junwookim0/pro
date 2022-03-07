d9a3e7cc6ed025ad6eb3db0ccf5ae990d865f83e
junwookim <ekdlskalrx98@naver.com>
2022-03-07 17:11:43 +0900

    0307 l?

---
+++ 김준우_자바스크립트객체향프로그래밍/todo.js
@@ -0,0 +1,56 @@
+const todoBoard = document.querySelector("#todolistboard");
+const todoBtn = document.querySelector("#todolistBtn");
+const todoInput = document.querySelector("#todo-form input")
+const p =document.querySelector("p");
+
+todoBtn.addEventListener("click",todoB);
+
+function todoB(){
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("click", todocheck);
+    checkbox.addEventListener("click", ckd);
+
+    const text = document.createTextNode(todoInput.value);
+
+    const button = document.createElement("button");
+    button.textContent = "x";
+    button.addEventListener("click",del);
+    function del(event){
+        li.remove()
+        if(checkbox.checked){
+            count--;
+        }
+        p.textContent=`오늘완료한일 : ${count}`;
+    }
+
+
+    li.appendChild(checkbox);
+    li.appendChild(text);
+    li.appendChild(button);
+    todoBoard.appendChild(li);
+    todoInput.value = "";
+}
+function todocheck(event){
+    const checkbox = event.target;
+    if(checkbox.checked){
+        checkbox.parentNode.style.color = "gray";
+    }else{
+        checkbox.parentNode.style.color = "black";
+    } 
+}
+
+let count =0;
+function ckd(){
+    count++;
+    let p =document.querySelector("p");
+    const li = document.querySelectorAll("li");
+    const checkbox = event.target;
+    if(checkbox.checked){
+    }
+    else{
+        count = count-2;
+    }
+    p.textContent = `오늘완료한일 : ${count}`;
+}
