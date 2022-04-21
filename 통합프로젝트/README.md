47953a1ec694a3ecc23f01e194819e6c0ec8316a
kimjunwoo <ekdlskalrx98@naver.com>
2022-04-21 18:26:43 +0900

    backend+frontend

---
+++ 통합프로젝트/README.md
@@ -0,0 +1,29 @@
+# 순서
+```
+frontend 만들고
+backend 만들기
+npm install express-generator -g  
+express --no-view (생성폴더명)  or express --view=ejs (생성폴더명)
+
+frontend 폴더에서 
+vue.config.js 파일 생성
+const path = require('path')
+
+module.exports = {
+    //build 파일 위치를 backend의 public에 생성
+    outputDir : path.resolve(__dirname, '../backend/public'),
+};
+으로 연결
+npm run build 해서 연결
+
+backend 폴더에서 
+npn install 
+SET DEBUG=expresstest:* & npm start 
+으로 실행 
+
+npm install connect-history-api-fallback -save router 연결 
+backend router index.js 에서 public를 통해 html을 불러오도록 함 
+
+frontend 에서 npm install axios --save
+
+```
---
+++ 통합프로젝트/frontend/src/views/AboutView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="about">
+    <h1>메모</h1>
+    <table v-for="(memo, i) in memo" :key="i">
+      <tr>
+        <td>{{memo.id}}</td>
+        <td>{{memo.writer}}</td>
+        <!--title을 클릭 했을 때 pageview 화면으로 넘어감 -->
+        <!-- 보여지는 화면은 vue의 화면이기에 뷰컴포넌트와 라우터가 존재해야함 -->
+        <td @click="$router.push(`/about/${memo.id}`)">{{memo.title}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+/*
+  [2] about링크를 눌렀을 때 AboutView 컴포넌트 호풀
+  [3] axios를 통해서 백엔드(/api/memo)에  http 객체로 request 보냄
+*/
+export default {
+  //11. created() : 뷰 컴포넌트가 실행될때 (생성) 마다 아래 함수를 실행하기 위함
+  created() { 
+    // 메모값을 vue가 생성될때 들고옴 -axios는  fetch와 사용법 유사
+    // 주소 '/api/memo'로 get메소드로 request를 보냄
+    this.$http.get('/api/memo')
+    .then((response)=>{
+      console.log(response.data)
+      //12. response 값을 memo에 저장 
+      this.memo = response.data
+    })
+
+    /*
+      [6] memo.js 에서 보내준 emomos의 값을 response를 통해 전달받음
+    */
+  },
+  data() {
+    return {
+      memo :[]
+    }
+  }
+}
+</script>
