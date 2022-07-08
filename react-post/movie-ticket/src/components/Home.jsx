96d3d93fba3ee029efb75a2d0a69a392e58c8acc
kimjunwoo <ekdlskalrx98@naver.com>
2022-07-08 13:34:51 +0900

    check pri

--- react-post/movie-ticket/src/components/Home.jsx
+++ react-post/movie-ticket/src/components/Home.jsx
@@ -2,54 +2,54 @@ import "./Home.css";
 import  { useState } from "react";
 
 const db = [
-    { id: 1,  isToggle: false ,price:"1000"},
-    { id: 2,  isToggle: false ,price:"1000"},
-    { id: 3,  isToggle: false ,price:"1000"},
-    { id: 4,  isToggle: false ,price:"1000"},
-    { id: 5,  isToggle: false ,price:"1000"},
-    { id: 6,  isToggle: false ,price:"1000"},
-    { id: 7,  isToggle: false ,price:"1000"},
-    { id: 8,  isToggle: false ,price:"1000"},
-    { id: 9,  isToggle: false ,price:"1000"},
-    { id: 10,  isToggle: false ,price:"1000"},
-    { id: 11,  isToggle: false ,price:"1000"},
-    { id: 12,  isToggle: false ,price:"1000"},
-    { id: 13,  isToggle: false ,price:"1000"},
-    { id: 14,  isToggle: false ,price:"1000"},
-    { id: 15,  isToggle: false ,price:"1000"},
-    { id: 16,  isToggle: false ,price:"1000"},
-    { id: 17,  isToggle: false ,price:"1000"},
-    { id: 18,  isToggle: false ,price:"1000"},
-    { id: 19,  isToggle: false ,price:"1000"},
-    { id: 20,  isToggle: false ,price:"1000"},
-    { id: 21,  isToggle: false ,price:"1000"},
-    { id: 22,  isToggle: false ,price:"1000"},
-    { id: 23,  isToggle: false ,price:"1000"},
-    { id: 24,  isToggle: false ,price:"1000"},
-    { id: 25,  isToggle: false ,price:"1000"},
-    { id: 26,  isToggle: false ,price:"1000"},
-    { id: 27,  isToggle: false ,price:"1000"},
-    { id: 28,  isToggle: false ,price:"1000"},
-    { id: 29,  isToggle: false ,price:"1000"},
-    { id: 30,  isToggle: false ,price:"1000"},
-    { id: 31,  isToggle: false ,price:"1000"},
-    { id: 32,  isToggle: false ,price:"1000"},
-    { id: 33,  isToggle: false ,price:"1000"},
-    { id: 34,  isToggle: false ,price:"1000"},
-    { id: 35,  isToggle: false ,price:"1000"},
-    { id: 36,  isToggle: false ,price:"1000"},
-    { id: 37,  isToggle: false ,price:"1000"},
-    { id: 38,  isToggle: false ,price:"1000"},
-    { id: 39,  isToggle: false ,price:"1000"},
-    { id: 40,  isToggle: false ,price:"1000"},
+    { id: 1,  isToggle: false},
+    { id: 2,  isToggle: false},
+    { id: 3,  isToggle: false},
+    { id: 4,  isToggle: false},
+    { id: 5,  isToggle: false},
+    { id: 6,  isToggle: false},
+    { id: 7,  isToggle: false},
+    { id: 8,  isToggle: false},
+    { id: 9,  isToggle: false},
+    { id: 10,  isToggle: false },
+    { id: 11,  isToggle: false },
+    { id: 12,  isToggle: false },
+    { id: 13,  isToggle: false },
+    { id: 14,  isToggle: false },
+    { id: 15,  isToggle: false },
+    { id: 16,  isToggle: false },
+    { id: 17,  isToggle: false },
+    { id: 18,  isToggle: false },
+    { id: 19,  isToggle: false },
+    { id: 20,  isToggle: false },
+    { id: 21,  isToggle: false },
+    { id: 22,  isToggle: false },
+    { id: 23,  isToggle: false },
+    { id: 24,  isToggle: false },
+    { id: 25,  isToggle: false },
+    { id: 26,  isToggle: false },
+    { id: 27,  isToggle: false },
+    { id: 28,  isToggle: false },
+    { id: 29,  isToggle: false },
+    { id: 30,  isToggle: false },
+    { id: 31,  isToggle: false },
+    { id: 32,  isToggle: false },
+    { id: 33,  isToggle: false },
+    { id: 34,  isToggle: false },
+    { id: 35,  isToggle: false },
+    { id: 36,  isToggle: false },
+    { id: 37,  isToggle: false },
+    { id: 38,  isToggle: false },
+    { id: 39,  isToggle: false },
+    { id: 40,  isToggle: false },
 ];
 const Menu = ({ id,  isToggle }) => {
     const [toggle, setToggle] = useState(isToggle);
-
+    const [pri, setPri] = useState([]);
     const onChange = () => {
         setToggle(!toggle);
+        setPri(10);
     };
-    
     return (
         <div>
             <div id={id} onClick={onChange}>
@@ -57,12 +57,16 @@ const Menu = ({ id,  isToggle }) => {
                     <span className="material-icons">square</span>
                 </div>
             </div>
+                <div>
+                    {pri}
+                </div>
         </div>
     );
 };
 
 function Home() {
     const [list] = useState(db);
+
     return (
         <div>
             <div className="home_box">
@@ -79,7 +83,7 @@ function Home() {
                             <option value="8000">Toy Story4 (8,000원)</option>
                             <option value="9000">스파이더맨 파프롬홈 (9,000원)</option>
                         </select>
-                    </div> 
+                    </div>
                     <div className="movie-seat-ex">
                         <div className="available"><span className="material-icons">square</span>
                             <div id="ex-text">선택가능</div>
