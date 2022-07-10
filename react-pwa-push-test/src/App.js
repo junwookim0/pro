cd456b138c575d08a6f6d7e6f62e27370db59dde
kimjunwoo <ekdlskalrx98@naver.com>
2022-07-10 12:04:14 +0900

    push test

---
+++ react-pwa-push-test/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>프로그레시브 웹앱</h1>
+        <p>푸시 테스트를 하고 있습니다</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
